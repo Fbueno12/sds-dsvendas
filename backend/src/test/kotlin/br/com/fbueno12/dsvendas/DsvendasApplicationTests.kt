d8b7c463fc7a026fa60c4a847632023ba07fe5a5
Felipe Bueno <felipebueno181@gmail.com>
2021-05-03 19:31:05 -0300

    iniciando projetos front e backend

---
+++ backend/src/test/kotlin/br/com/fbueno12/dsvendas/DsvendasApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fbueno12.dsvendas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
